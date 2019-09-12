c42c9fdadb69299e8dfc6695914895933b218778
Rachel Kennelly <rekennelly@gmail.com>
2019-09-12 14:40:56 -0400

    add notes for 9/12/19

---
+++ Class_Sept12/Class_Sept12/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  Class_Sept12
+//
+//  Created by Rachel Kennelly on 9/12/19.
+//  Copyright © 2019 Rachel Kennelly. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    // Single quote or apostrophe for characters
+    char myFirstLetter = 'E';
+    char mySecondLetter = 'R';
+    char myThirdLetter = 'I';
+    char myFourthLetter = 'C';
+    
+    // double quotes for strings
+    string name = "ERIC";
+    
+    cout << myFirstLetter << mySecondLetter << myThirdLetter << myFourthLetter << endl;
+    
+    cout << name << endl;
+    
+    return 0;
+}
