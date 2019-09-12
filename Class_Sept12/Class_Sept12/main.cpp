b5497f2a7940f965bae706fd7c43d9ae04b4956a
Rachel Kennelly <rekennelly@gmail.com>
2019-09-12 15:03:45 -0400

    added madlibs

--- Class_Sept12/Class_Sept12/main.cpp
+++ Class_Sept12/Class_Sept12/main.cpp
@@ -3,10 +3,10 @@
 //  Class_Sept12
 //
 //  Created by Rachel Kennelly on 9/12/19.
-//  Copyright © 2019 Rachel Kennelly. All rights reserved.
 //
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -19,11 +19,69 @@ int main() {
     char myFourthLetter = 'C';
     
     // double quotes for strings
-    string name = "ERIC";
+    // \t adds a tab
+    // \" adds quotation marks in a string
+    
+    // \n adds a new line, but it is UGLY
+    string name = "Hassan \"Z\" Z";
+    
+    // string classroom = "Hassan \nEric \nRachel";
+    // cout << classroom << endl;
+    
+    string ericsName = "Eric \"Mr. C\"";
+    string rachelsName = "Rachel";
+    
+    myFourthLetter = 'E';
     
     cout << myFirstLetter << mySecondLetter << myThirdLetter << myFourthLetter << endl;
     
-    cout << name << endl;
+    cout << name << endl << ericsName << endl << rachelsName << endl;
+    
+    cout << "Enter your name: " << endl;
+    
+    cin >> name;
+    
+    cout << "Hi there, " << name << endl << endl;
+    
+    /* ***********
+     *  Madlibs  *
+     *************/
+    
+    cout << "Let's make a madlib!" << endl;
+    
+    string madlibName;
+    string madlibTheme;
+    string madlibPlace;
+    string madlibDay;
+    string madlibTime;
+    string madlibVerb;
+    string madlibAnimal;
+    
+    cout << "Enter a name: " << endl;
+    cin >> madlibName;
+    
+    cout << "Enter a theme: " << endl;
+    cin >> madlibTheme;
+    
+    cout << "Enter a place: " << endl;
+    cin >> madlibPlace;
+    
+    cout << "Enter a day of the week: " << endl;
+    cin >> madlibDay;
+    
+    cout << "Enter a time: " << endl;
+    cin >> madlibTime;
+    
+    cout << "Enter a verb: " << endl;
+    cin >> madlibVerb;
+    
+    cout << "Enter an animal: " << endl;
+    cin >> madlibAnimal;
+    
+    cout << "Let's see how your madlib turned out!" << endl << endl;
+    cout << madlibName << " is having a " << madlibTheme << " party!" << endl;
+    cout << "It's going to be at " << madlibPlace << " on " << madlibDay << "." << endl;
+    cout << "Please show up at " << madlibTime << " or else you will be required to " << madlibVerb << " a " << madlibAnimal << "." << endl;
     
     return 0;
 }
