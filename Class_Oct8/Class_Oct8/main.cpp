0315b9f4389e0cbdf9a965f224384dd84624ff08
Rachel Kennelly <rekennelly@gmail.com>
2019-10-08 15:07:34 -0400

    more function notes

--- Class_Oct8/Class_Oct8/main.cpp
+++ Class_Oct8/Class_Oct8/main.cpp
@@ -6,6 +6,10 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+// ^^ these are pre-processor directives
 
 using namespace std;
 
@@ -13,24 +17,73 @@ int luckyNumber();
 int doublyLuckyNumber(int number);
 string capitalizeString(string name);
 
+double getAverageScore(vector<int> scores);
+double getMaxScore(vector<int> scores);
+double getMinScore(vector<int> scores);
+double getPopulationStandardDeviation(vector<int> scores);
+
+// overloading the function -- 2 functions with same names & different parameters
+void printTheDate(int day, int month, int year, int printStyle = 0) {
+    if (printStyle == 0) {
+        cout << month << "-" << day << "-" << year << endl;
+    }
+    else if (printStyle == 1) {
+        cout << day << "-" << month << "-" << year << endl;
+    }
+    else {
+        cout << year << "-" << month << "-" << day << endl;
+    }
+}
+
+void printTheDate(string day, string month, string year, int printStyle = 0) {
+    printTheDate(stoi(day), stoi(month), stoi(year), printStyle);
+}
+
+// avoid using global variables if you can
+
 int main() {
+//    
+//    int usersNumber;
+//    cout << "Pick a number, any number" << endl;
+//    cin >> usersNumber;
+//    
+//    int luckyNumber;
+//    
+//    luckyNumber = doublyLuckyNumber(usersNumber);
+//    
+//    cout << luckyNumber << endl;
+//    
+//    cout << "Enter your name: ";
+//    string name;
+//    cin >> name;
+//    
+//    name = capitalizeString(name);
+//    cout << name << endl;
     
-    int usersNumber;
-    cout << "Pick a number, any number" << endl;
-    cin >> usersNumber;
+    vector<int> scores;
     
-    int luckyNumber;
+    int score = 0;
+    while (score != -1) {
+        cout << "Enter a score or -1 to stop" << endl;
+        cin >> score;
+        if (score != -1) {
+            scores.push_back(score);
+        }
+    }
     
-    luckyNumber = doublyLuckyNumber(usersNumber);
+    double averageScore = getAverageScore(scores);
+    double minScore = getMinScore(scores);
+    double maxScore = getMaxScore(scores);
+    double standardDeviation = getPopulationStandardDeviation(scores);
     
-    cout << luckyNumber << endl;
+    cout << "Average score: " << averageScore << endl;
+    cout << "Min Score: " << minScore << endl;
+    cout << "Max Score: " << maxScore << endl;
+    cout << "Standard Deviation: " << standardDeviation << endl;
     
-    cout << "Enter your name: ";
-    string name;
-    cin >> name;
-    
-    name = capitalizeString(name);
-    cout << name << endl;
+    printTheDate(8, 10, 2019);
+    printTheDate(8, 10, 2019, 1);
+    printTheDate(8, 10, 2019, 2);
     
     return 0;
 }
@@ -46,4 +99,50 @@ int doublyLuckyNumber(int number) {
 string capitalizeString(string name) {
     name.at(0) = toupper(name.at(0));
     return name;
+}
+
+double getAverageScore(vector<int> scores) {
+    double total = 0;
+    for (int index = 0; index < scores.size(); index++ ) {
+        total += scores.at(index);
+    }
+    return total / scores.size();
+}
+
+double getMinScore(vector<int> scores) {
+    int minScore = scores.at(0);
+    for (int index = 1; index < scores.size(); index++ ) {
+        if (scores.at(index) < minScore) {
+            minScore = scores.at(index);
+        }
+    }
+    return minScore;
+}
+
+double getMaxScore(vector<int> scores) {
+    int maxScore = scores.at(0);
+    for (int index = 1; index < scores.size(); index++ ) {
+        if (scores.at(index) > maxScore) {
+            maxScore = scores.at(index);
+        }
+    }
+    return maxScore;
+}
+
+double getPopulationStandardDeviation(vector<int> scores) {
+    
+    double averageScore = getAverageScore(scores);
+    
+    double totalOfDifferenceFromAverageSquared = 0;
+    
+    for (int index = 0; index < scores.size(); index++) {
+        double difference = scores.at(index) - averageScore;
+        double differenceSquared = difference * difference;
+        
+        totalOfDifferenceFromAverageSquared += differenceSquared;
+    }
+    
+    double variance = totalOfDifferenceFromAverageSquared / scores.size();
+    
+    return sqrt(variance);
 }
