456c9043beed0415738baad56fd8b3428e9ed4f6
Rachel Kennelly <rekennelly@gmail.com>
2019-10-29 10:13:55 -0400

    created oct24 notes (notes from stream)

---
+++ Class_Oct24/Class_Oct24/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Class_Oct24
+//
+//  Created by Rachel Kennelly on 10/29/19.
+//  Copyright © 2019 Rachel Kennelly. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+
+    return 0;
+}
