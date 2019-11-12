23d03fbb5a3f6d70548e48f59a1fa25f3f32578e
Rachel Kennelly <rekennelly@gmail.com>
2019-11-12 15:13:28 -0500

    not many notes

---
+++ Class_Nov5/Class_Nov5/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Class_Nov5
+//
+//  Created by Rachel Kennelly on 11/5/19.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << -1 % 26 << endl;
+    
+    
+    return 0;
+}
